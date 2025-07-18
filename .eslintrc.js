859c044734f51dbdbc963a0ae1b11eeffa12fea8
Nikita_Kucherenko <ext_nkucherenko@rencredit.ru>
2025-07-19 01:21:38 +0300

    disable eslint temporarily

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,12 +1,26 @@
 module.exports = {
     extends: ['react-app', 'react-app/jest'],
     rules: {
-      // отключим то, что не критично
-      'no-unused-vars': 'warn',
-      'no-useless-constructor': 'warn',
-      'eqeqeq': 'warn',
-      'no-redeclare': 'warn',
-      'jsx-a11y/anchor-is-valid': 'warn',
-      'react/no-direct-mutation-state': 'warn'
-    }
+      // отключаем самые «шумные» правила
+      'no-useless-constructor': 'off',
+      'react/no-direct-mutation-state': 'off',
+      'jsx-a11y/anchor-is-valid': 'off',
+      'no-unused-vars': 'off',
+      eqeqeq: 'off',
+      'no-redeclare': 'off',
+    },
+    overrides: [
+      {
+        // для всего, что сгенерировано Swagger-Codegen
+        files: ['src/server-api/**/*.js'],
+        env: { node: true },
+        rules: {
+          // полностью выключить ESLint
+          'no-console': 'off',
+          'no-undef': 'off',
+          'no-unused-vars': 'off',
+          'eqeqeq': 'off',
+        },
+      },
+    ],
   };
