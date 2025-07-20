77d42596753f9a57e60733c41de325025d0b7c91
Nikita_Kucherenko <ext_nkucherenko@rencredit.ru>
2025-07-20 15:12:43 +0300

    fix variables for react

--- src/config/env.config.js
+++ src/config/env.config.js
@@ -1,5 +1,5 @@
 export const env = {
-    basename:   process.env.LANDING_APP_BASENAME || '/',
-    backendUrl:     process.env.LANDING_APP_BACKEND_URL  || 'http://localhost:8085',
+    basename:   process.env.REACT_APP_LANDING_APP_BASENAME || '/',
+    backendUrl:     process.env.REACT_APP_LANDING_APP_BACKEND_URL  || 'http://localhost:8085',
     isDev:      process.env.NODE_ENV === 'development',
   };
